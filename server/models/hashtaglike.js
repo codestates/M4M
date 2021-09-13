6a4d49a50d6d2b2b3f2dc67f99c30c0ea1167392
Kyungjoo Ha <429why@gmail.com>
2021-09-13 21:48:45 +0900

    modify: Models

--- server/models/hashtaglike.js
+++ server/models/hashtaglike.js
@@ -9,18 +9,18 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
       hashtaglike.hasMany(models.songuserhashtaglike, {
-        foreignKey: "hashtagId",
+        foreignKey: 'hashtagId'
       });
     }
-  };
+  }
   hashtaglike.init({
     name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'hashtaglike',
+    modelName: 'hashtaglike'
   });
   return hashtaglike;
 };
