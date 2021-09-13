81e9c9a1dc92201accf0251a66717376ba084879
Kyungjoo Ha <429why@gmail.com>
2021-09-13 21:49:23 +0900

    add: Search controller

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const { findAll } = require('./controllers/test/searchTest');
+
+const controllers = require('./controllers');
+const { findAllTitle } = require('./controllers/search/title');
+const { findAllArtist } = require('./controllers/search/artist');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,10 +22,12 @@ app.use(
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-app.get('/searchTest', findAll);
+// 배포 테스트용
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
+app.get('/title', findAllTitle);
+app.get('/artist', findAllArtist);
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 80;
 
