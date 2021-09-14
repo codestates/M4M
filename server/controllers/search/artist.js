d82c77b982276160d80f3921839779ec51ef04af
Kyungjoo Ha <429why@gmail.com>
2021-09-14 12:13:02 +0900

    modify: Search controllers

--- server/controllers/search/artist.js
+++ server/controllers/search/artist.js
@@ -1,11 +1,11 @@
-const { song } = require('../../models');
+const { song, hashtaglike, songuserhashtaglike } = require('../../models');
 const Sequelize = require('sequelize');
 require('sequelize-values')(Sequelize);
 const Op = Sequelize.Op;
 
 module.exports = {
-  // 예시(http 환경일 때): http://localhost:80/artist?query=일기예보
-  // 예시(https 환경일 때): https://localhost:80/artist?query=일기예보
+  // 예시(http 환경일 때): http://localhost:80/artist?query=윤종신
+  // 예시(https 환경일 때): https://localhost:80/artist?query=윤종신
   findAllArtist: async (req, res) => {
     try {
       if (req.query.query !== undefined) {
@@ -18,7 +18,7 @@ module.exports = {
           });
         }
 
-        let songInfo = await song.findAll({
+        let songList = await song.findAll({
           where: {
             artist: {
               [Op.like]: '%' + req.query.query + '%'
@@ -26,13 +26,14 @@ module.exports = {
           }
         });
 
-        if (songInfo.length === 0) {
+        if (songList.length === 0) {
           res.status(400).json({
             message: 'Artist not found'
           });
         } else {
-          songInfo = Sequelize.getValues(songInfo);
-          songInfo = songInfo.map((song) => {
+          songList = Sequelize.getValues(songList);
+
+          songList = songList.map((song) => {
             song.artist = song.artist.replace(/[|]/g, ',');
 
             return {
@@ -43,11 +44,63 @@ module.exports = {
               date: song.date
             };
           });
-          console.log(songInfo);
-          res.status(200).json({
-            data: songInfo,
-            message: 'ok'
-          });
+
+          const fetchSongInfo = async () => {
+            const songInfo = songList.map(async (song) => {
+              // console.log("++++++++++++++++++\n", song.id);
+              try {
+                let getHashtagName = await songuserhashtaglike.findAll({
+                  include: [{
+                    model: hashtaglike,
+                    attributes: ['name']
+                  }],
+                  where: {
+                    songId: song.id
+                  }
+                });
+
+                getHashtagName = Sequelize.getValues(getHashtagName);
+                // console.log(getHashtagName);
+
+                const hashtaglikeCount = {};
+                getHashtagName.map((song) => {
+                  // console.log(song.hashtaglike.name);
+                  if (hashtaglikeCount[song.hashtaglike.name]) {
+                    hashtaglikeCount[song.hashtaglike.name] += 1;
+                  } else {
+                    hashtaglikeCount[song.hashtaglike.name] = 1;
+                  }
+                });
+
+                // console.log(hashtaglikeCount);
+                
+                if (!hashtaglikeCount['좋아요']) {
+                  hashtaglikeCount['좋아요'] = 0;
+                }
+
+                return {
+                  id: song.id,
+                  title: song.title,
+                  artist: song.artist,
+                  album_art: song.album_art,
+                  date: song.date,
+                  hashtagLike: hashtaglikeCount
+                };
+              } catch (error) {
+                console.log(error);
+              }
+            });
+
+            const results = await Promise.all(songInfo);
+            // console.log(results);
+
+            res.status(200).json({
+              data: results,
+              message: 'ok'
+            });
+          };
+
+          fetchSongInfo();
         }
       }
     } catch (error) {
--- server/controllers/search/title.js
+++ server/controllers/search/title.js
@@ -1,15 +1,15 @@
-const { song } = require('../../models');
+const { song, hashtaglike, songuserhashtaglike } = require('../../models');
 const Sequelize = require('sequelize');
 require('sequelize-values')(Sequelize);
 const Op = Sequelize.Op;
 
 module.exports = {
-  // 예시(http 환경일 때): http://localhost:80/title?query=말해줘
-  // 예시(https 환경일 때): https://localhost:80/title?query=말해줘
+  // 예시(http 환경일 때): http://localhost:80/title?query=그럴때마다
+  // 예시(https 환경일 때): https://localhost:80/title?query=그럴때마다
   findAllTitle: async (req, res) => {
     try {
       if (req.query.query !== undefined) {
-        console.log(req.query.query);
+        console.log('query: ' + req.query.query);
 
         // 쿼리가 비어있을 때
         if (req.query.query.length === 0) {
@@ -18,7 +18,7 @@ module.exports = {
           });
         }
 
-        let songInfo = await song.findAll({
+        let songList = await song.findAll({
           where: {
             title: {
               [Op.like]: '%' + req.query.query + '%'
@@ -26,13 +26,14 @@ module.exports = {
           }
         });
 
-        if (songInfo.length === 0) {
+        if (songList.length === 0) {
           res.status(400).json({
             message: 'Title not found'
           });
         } else {
-          songInfo = Sequelize.getValues(songInfo);
-          songInfo = songInfo.map((song) => {
+          songList = Sequelize.getValues(songList);
+
+          songList = songList.map((song) => {
             song.artist = song.artist.replace(/[|]/g, ',');
 
             return {
@@ -43,11 +44,63 @@ module.exports = {
               date: song.date
             };
           });
-          console.log(songInfo);
-          res.status(200).json({
-            data: songInfo,
-            message: 'ok'
-          });
+
+          const fetchSongInfo = async () => {
+            const songInfo = songList.map(async (song) => {
+              // console.log("++++++++++++++++++\n", song.id);
+              try {
+                let getHashtagName = await songuserhashtaglike.findAll({
+                  include: [{
+                    model: hashtaglike,
+                    attributes: ['name']
+                  }],
+                  where: {
+                    songId: song.id
+                  }
+                });
+
+                getHashtagName = Sequelize.getValues(getHashtagName);
+                // console.log(getHashtagName);
+
+                const hashtaglikeCount = {};
+                getHashtagName.map((song) => {
+                  // console.log(song.hashtaglike.name);
+                  if (hashtaglikeCount[song.hashtaglike.name]) {
+                    hashtaglikeCount[song.hashtaglike.name] += 1;
+                  } else {
+                    hashtaglikeCount[song.hashtaglike.name] = 1;
+                  }
+                });
+                
+                // console.log(hashtaglikeCount);
+
+                if (!hashtaglikeCount['좋아요']) {
+                  hashtaglikeCount['좋아요'] = 0;
+                }
+
+                return {
+                  id: song.id,
+                  title: song.title,
+                  artist: song.artist,
+                  album_art: song.album_art,
+                  date: song.date,
+                  hashtagLike: hashtaglikeCount
+                };
+              } catch (error) {
+                console.log(error);
+              }
+            });
+
+            const results = await Promise.all(songInfo);
+            // console.log(results);
+
+            res.status(200).json({
+              data: results,
+              message: 'ok'
+            });
+          };
+
+          fetchSongInfo();
         }
       }
     } catch (error) {
