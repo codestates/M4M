02ca7cc5fa95cdc77f7d39d0bcb7579d58f83b5a
magababo <magababo@naver.com>
2021-09-15 13:56:34 +0900

    add & delete & modify: font add & react css delete & detail modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
-import './App.css';
+import styled from 'styled-components';
 import { BrowserRouter } from "react-router-dom";
+import GlobalFont from "./font/font";
 import Header from './basic_component/Header';
 import Footer from './basic_component/Footer';
+
+const AppWrapper = styled.div`
+  div,
+  button,
+  select,
+  input {
+    font-family: 'neodgm';
+  }
+  .App {
+    margin: 0;
+    text-align: center;
+  }
+`
 
 function App () {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
+      <AppWrapper>
+        <GlobalFont />
+        <div className="App">
+          <Header />
+          <Footer />
+        </div>
+      </AppWrapper>
     </BrowserRouter>
   );
 }
