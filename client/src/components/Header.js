f322747b595856f59ce1e692cf1acd7d41e1b817
magababo <magababo@naver.com>
2021-09-16 20:30:37 +0900

    delete: state  test button delete

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -59,44 +59,37 @@ function Header () {
 
   return (
     <HeaderWrapper>
-      <>
-        <div className='header'>
-          <div className='header-container-1'>
-            <Link to='/mainpage'>
-              <div className='logo'>M4M Logo</div>
-            </Link>
-          </div>
-          <div className='header-container-2'>
-            <Link to='/recommendpage'>
-              <button className='btn recommend-page' disabled={isRecommend ? 'disabled' : null}>recommend page</button>
-            </Link>
-          </div>
-          <div className='header-container-3'>
-            <HeaderSearchbar />
-          </div>
-          <div className='header-container-4'>
-            {!isLogin
-              ? <Link to='/login'>
-                <button className='btn login'>login</button>
-                </Link>
-              : <Link to='/logout'>
-                <button className='btn logout'>logout</button>
-              </Link>}
-            {!isLogin
-              ? <Link to='/signup'>
-                <button className='btn signup'>signup</button>
-                </Link>
-              : <Link to='/mypage'>
-                <button className='btn mypage'>mypage</button>
-              </Link>}
-          </div>
+      <div className='header'>
+        <div className='header-container-1'>
+          <Link to='/mainpage'>
+            <div className='logo'>M4M Logo</div>
+          </Link>
         </div>
-        <div className='test-container'>
-          <button className='btn test' onClick={handleIsLogin}>change login status for test</button>
-          <button className='btn test' onClick={handleIsRecommend}>change Recommend status for test</button>
-          <p>현재 HeaderSearchbar 미적용. Redux 사용 시 추가 예정</p>
+        <div className='header-container-2'>
+          <Link to='/recommendpage'>
+            <button className='btn recommend-page' disabled={isRecommend ? 'disabled' : null}>recommend page</button>
+          </Link>
         </div>
-      </>
+        <div className='header-container-3'>
+          <HeaderSearchbar />
+        </div>
+        <div className='header-container-4'>
+          {!isLogin
+            ? <Link to='/login'>
+              <button className='btn login'>login</button>
+              </Link>
+            : <Link to='/logout'>
+              <button className='btn logout'>logout</button>
+            </Link>}
+          {!isLogin
+            ? <Link to='/signup'>
+              <button className='btn signup'>signup</button>
+              </Link>
+            : <Link to='/mypage'>
+              <button className='btn mypage'>mypage</button>
+            </Link>}
+        </div>
+      </div>
     </HeaderWrapper>
   );
 }
