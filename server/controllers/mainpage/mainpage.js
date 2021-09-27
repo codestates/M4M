9fa482c395030900c58ebb04de091af805159fda
magababo <magababo@naver.com>
2021-09-27 22:12:46 +0900

    modify: Data format sent to client

--- server/controllers/mainpage/mainpage.js
+++ server/controllers/mainpage/mainpage.js
@@ -1,9 +1,13 @@
+const { isAuthorized } = require('../tokenFunctions');
 const { song, hashtaglike, songuserhashtaglike } = require('../../models');
 const Sequelize = require('sequelize');
 
 module.exports = async (req, res) => {
   try {
-    let songList = await song.findAll({});
+    // const accessTokenData = isAuthorized(req);
+    // JUST FOR TEST PURPOSES: without a real accessToken
+    const accessTokenData = { id: req.headers.authorization };
+    let songList = await song.findAll();
 
     if (songList.length === 0) {
       res.status(400).json({
@@ -15,13 +19,17 @@ module.exports = async (req, res) => {
       songList = songList.map((song) => {
         song.title = song.title.replace(/[|]/g, ',');
         song.artist = song.artist.replace(/[|]/g, ',');
+        song.album = song.album.replace(/[|]/g, ',');
+        song.genre = song.genre.replace(/[|]/g, ',');
 
         return {
           id: song.id,
           title: song.title,
           artist: song.artist,
+          genre: song.genre,
           album_art: song.album_art,
-          date: song.date
+          date: song.date,
+          year: song.year
         };
       });
 
@@ -43,7 +51,14 @@ module.exports = async (req, res) => {
             // console.log(getHashtagName);
 
             let hashtaglikeCount = {
-              좋아요: 0
+              좋아요: 0,
+              '#인생곡인': 0,
+              '#가사가재밌는': 0,
+              '#몸이기억하는': 0,
+              '#눈물샘자극': 0,
+              '#노래방금지곡': 0,
+              '#영원한18번': 0,
+              '#추억소환': 0
             };
 
             getHashtagName.map((song) => {
@@ -55,29 +70,48 @@ module.exports = async (req, res) => {
               }
             });
 
-            // 80번째 줄 주석 해제로 hashtagLike 현재 타입 확인
             // hashtagLike 배열 형태로 출력, 객체로 출력해야할 시 주석 처리 후 실행
             hashtaglikeCount = Object.entries(hashtaglikeCount);
 
-            return {
+            const payload = {
               id: song.id,
               title: song.title,
               artist: song.artist,
+              genre: song.genre,
               album_art: song.album_art,
               date: song.date,
+              year: song.year,
               hashtagLike: hashtaglikeCount
             };
+
+            // 로그인 된 유저에 한해서는 본인이 추가한 좋아요 및 해시태그 정보를 추가적으로 보내주어야 함.
+            if (accessTokenData.id) {
+              let userLike = await songuserhashtaglike.findAll(
+                {
+                  where: {
+                    userId: accessTokenData.id,
+                    songId: song.id,
+                    hashtagId: 1
+                  }
+                }
+              );
+
+              userLike = Sequelize.getValues(userLike);
+
+              if (userLike.length > 0) {
+                payload.userLike = true;
+              } else {
+                payload.userLike = false;
+              }
+            }
+
+            return payload;
           } catch (error) {
-            console.log(error);
+            res.status(400).json({ message: 'Error' });
           }
         });
 
         const results = await Promise.all(songInfo);
-        // 전체 결과 출력
-        // console.log(results);
-
-        // 현재 hashtagLike의 타입 조회
-        // Array.isArray(results[0].hashtagLike) ? console.log('배열') : console.log('객체');
 
         res.status(200).json({
           data: results,
@@ -88,6 +122,6 @@ module.exports = async (req, res) => {
       fetchSongInfo();
     }
   } catch (error) {
-    console.error(error);
+    res.status(400).json({ message: 'Error' });
   }
 };
