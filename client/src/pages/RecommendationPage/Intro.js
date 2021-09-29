bd36e0af34c4ce8e0da248b69793c83f387acf9d
Kyungjoo Ha <429why@gmail.com>
2021-09-30 07:43:59 +0900

    modify: Test1

--- client/src/pages/RecommendationPage/Intro.js
+++ client/src/pages/RecommendationPage/Intro.js
@@ -45,6 +45,7 @@ const Intro = ({ _onStartClick }) => {
         <ul className='list-group'>
           <li className='list-item'>밀레니얼 세대를 위한<br />노래 추천</li>
         </ul>
+        <div>다시 테스트</div>
         <StartBtn onClick={_onStartClick}>
           <span>테스트 시작!</span>
         </StartBtn>
