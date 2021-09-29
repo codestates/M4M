c7f63275e280f5f77c1e6e0a2243e4ad3edd59b5
Kyungjoo Ha <429why@gmail.com>
2021-09-29 18:48:19 +0900

    modify: Get prepared for the deployment

--- server/controllers/songs/song.js
+++ server/controllers/songs/song.js
@@ -10,9 +10,7 @@ const Sequelize = require('sequelize');
 
 module.exports = async (req, res) => {
   try {
-    // const accessTokenData = isAuthorized(req);
-    // JUST FOR TEST PURPOSES: without a real accessToken
-    const accessTokenData = { id: req.headers.authorization };
+    const accessTokenData = isAuthorized(req);
 
     const songs = req.query.query;
 
@@ -26,10 +24,22 @@ module.exports = async (req, res) => {
       });
     } else {
       // replace('|', ',') => 최초 1개만 변경, replace(/[|]/g, ',') => 모든 occurrence를 변경
-      songList.dataValues.title = songList.dataValues.title.replace(/[|]/g, ',');
-      songList.dataValues.artist = songList.dataValues.artist.replace(/[|]/g, ',');
-      songList.dataValues.genre = songList.dataValues.genre.replace(/[|]/g, ',');
-      songList.dataValues.album = songList.dataValues.album.replace(/[|]/g, ',');
+      songList.dataValues.title = songList.dataValues.title.replace(
+        /[|]/g,
+        ','
+      );
+      songList.dataValues.artist = songList.dataValues.artist.replace(
+        /[|]/g,
+        ','
+      );
+      songList.dataValues.genre = songList.dataValues.genre.replace(
+        /[|]/g,
+        ','
+      );
+      songList.dataValues.album = songList.dataValues.album.replace(
+        /[|]/g,
+        ','
+      );
 
       let getHashtagName = await songuserhashtaglike.findAll({
         include: [
@@ -69,9 +79,7 @@ module.exports = async (req, res) => {
           }
         ],
         where: { id: songs },
-        order: [
-          [comment, 'createdAt', 'DESC']
-        ]
+        order: [[comment, 'createdAt', 'DESC']]
       });
 
       getComment = Sequelize.getValues(getComment);
@@ -87,9 +95,14 @@ module.exports = async (req, res) => {
         ]);
       });
 
+      const flattenedUserId = getUserId.reduce(
+        (acc, val) => acc.concat(val),
+        []
+      );
+
       const userNickname = [];
-      for (let i = 0; i < getUserId.flat().length; i++) {
-        userNickname.push([getUserId.flat()[i][0]]);
+      for (let i = 0; i < flattenedUserId.length; i++) {
+        userNickname.push([flattenedUserId[i][0]]);
       }
 
       const getUserNicknames = [];
@@ -107,15 +120,18 @@ module.exports = async (req, res) => {
         getUserNickname.push(nickname.dataValues.nickname);
       }
 
-      for (let i = 0; i < getContent.flat().length; i++) {
-        if (getContent.flat()[i][0] !== getUserNickname[i]) {
-          getContent.flat()[i].unshift(getUserNickname[i]);
+      const flattenedContent = getContent.reduce(
+        (acc, val) => acc.concat(val),
+        []
+      );
+
+      for (let i = 0; i < flattenedContent.length; i++) {
+        if (flattenedContent[i][0] !== getUserNickname[i]) {
+          flattenedContent[i].unshift(getUserNickname[i]);
         } else {
           continue;
         }
       }
-
-      // console.log(getContent.flat());
 
       const payload = {
         id: songList.dataValues.id,
@@ -128,19 +144,17 @@ module.exports = async (req, res) => {
         year: songList.dataValues.year,
         lyrics: songList.dataValues.lyrics,
         hashtagLike: hashtaglikeCount,
-        comments: getContent.flat()
+        comments: flattenedContent
       };
 
       // 로그인 된 유저에 한해서는 본인이 추가한 좋아요 및 해시태그 정보를 추가적으로 보내주어야 함.
       if (accessTokenData.id) {
-        let userContent = await songuserhashtaglike.findAll(
-          {
-            where: {
-              userId: accessTokenData.id,
-              songId: req.query.query
-            }
+        let userContent = await songuserhashtaglike.findAll({
+          where: {
+            userId: accessTokenData.id,
+            songId: songs
           }
-        );
+        });
 
         userContent = Sequelize.getValues(userContent);
 
