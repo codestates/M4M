13e2bc1a8a421c294891b2b3360b60e4c045e543
Kyungjoo Ha <429why@gmail.com>
2021-10-05 18:32:05 +0900

    modify: CSS

---
+++ client/src/pages/RecommendationPage/Buttons.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { media } from '../../components/utils/_media-queries';
+import { Colors } from '../../components/utils/_var';
+
+const StartBtn = styled.div`
+  display: block;
+  /* border-radius: 5px; */
+  cursor: pointer;
+  /* background: ${Colors.pastelPurple}; */
+  border: 1px solid ${Colors.black};
+  margin: 1.5rem auto;
+  width: 7.5rem;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  font-family: 'Arial';
+  ${media.tabletMini`width: 8.2rem; height: 2.75rem; line-height: 2.75rem; margin: 2rem auto`}
+
+  span {
+    display: block;
+    line-height: inherit;
+    letter-spacing: 0.75px;
+    font-size: .8rem;
+    font-family: 'Arial';
+    font-family: 'NeoDunggeunmo';
+    ${media.tabletMini`font-size: .9rem`}
+    color: ${Colors.black};
+  }
+  &:hover {
+    background-color: ${Colors.black};
+    /* background: ${Colors.purple}; */
+    span {
+      color: white;
+    }
+  }
+`;
+
+export { StartBtn };
